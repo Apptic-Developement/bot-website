ce35c2570783d69ad50e906bacdfb3a28f016e3f
Pranoy Majumdar <kumarricky252@gmail.com>
2023-02-20 09:14:55 +0000

    Fixx a tag

--- components/Headers/Hero.tsx
+++ components/Headers/Hero.tsx
@@ -2,6 +2,7 @@
 import { FaDiscord } from "react-icons/fa";
 import { BsMouse } from "react-icons/bs";
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 // TODO ADD BACKGROUND DESIGN 
 // hero 
@@ -24,7 +25,7 @@ const Hero = () => {
 
                 <div className="flex mx-6 gap-1 flex-wrap items-center justify-center mt-6">
                     {/* invite button  */}
-                    <a href="/invite" target="_blank" className="cursor-pointer">
+                    <Link to="/invite" target="_blank" className="cursor-pointer">
                     <div className="flex duration-500 transition-all items-center justify-center gap-1 text-sm font-medium bg-gradient-to-r from-light-blurple to-dark-blurple rounded-3xl px-5 py-2.5 text-center mr-2 mb-2 hover:scale-110 hover:shadow-sm hover:shadow-dark-blurple">
                         <FaDiscord className="text-2xl" />
                         <button
@@ -32,7 +33,7 @@ const Hero = () => {
                             Add to server
                         </button>
                     </div>
-                    </a>
+                    </Link>
                     {/* scroll button  */}
                     <div className="flex items-center justify-center gap-1 text-sm font-medium bg-gradient-to-br hover:bg-gradient-to-bl rounded-3xl px-5 py-2.5 text-center mr-2 mb-2">
                         <BsMouse className="text-lg animate-bounce" />
